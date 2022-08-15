9ef60a037b18f7e1490635e4f9a23894c9ccc3a3
petprog <farinupeter@gmail.com>
2022-08-15 21:01:31 +0100

    add client to App widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:chatter/screens/screens.dart';
+import 'package:chatter/secrets.dart';
 import 'package:chatter/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  final client = StreamChatClient(streamKey);
+  runApp(
+    MyApp(
+      client: client,
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key, required this.client}) : super(key: key);
+
+  final StreamChatClient client;
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +25,12 @@ class MyApp extends StatelessWidget {
       darkTheme: AppTheme.dark(),
       themeMode: ThemeMode.light,
       title: 'Chatter',
+      builder: (context, child) {
+        return StreamChatCore(
+          client: client,
+          child: child!,
+        );
+      },
       home: HomeScreen(),
     );
   }
