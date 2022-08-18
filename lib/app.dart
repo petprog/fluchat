55cfc876e65a2388f0f192ddb9c7c4499ed31b49
petprog <farinupeter@gmail.com>
2022-08-18 09:06:04 +0100

    add an extension to get current user

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,13 @@
-import 'package:logger/logger.dart';
+import 'package:flutter/material.dart';
+import 'package:logger/logger.dart' as log;
+import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
 
-var logger = Logger();
+var logger = log.Logger();
+
+extension StreamChatContext on BuildContext {
+  /// Fetches the current user image.
+  String? get currentUserImage => currentUser!.image;
+
+  /// Fetches the current user.
+  User? get currentUser => StreamChatCore.of(this).currentUser;
+}
